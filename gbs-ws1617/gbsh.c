c199a6ddd0008bb017aff6a48076535ac556b8d2
Bora <bora.ozgen@punicasystems.com>
2016-11-14 00:04:29 +0100

    Add forking and executing functionality

--- gbs-ws1617/gbsh.c
+++ gbs-ws1617/gbsh.c
@@ -3,7 +3,7 @@
 //# 2. Owen Wilcox
 //# 3. Bora Özgen
 
-
+#define _GNU_SOURCE // for execvpe()
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,6 +16,7 @@
 
 // Linked list structure for environment variables
 struct envar {
+	// TODO: can we dynamically allocate these too? Also: only one string is enough eg. 'shell=path'
 	char variable[ENVAR_VAR_LENGTH + 1];
 	char value[ENVAR_VALUE_LENGTH + 1];
 	struct envar *next;
@@ -29,7 +30,10 @@ void print_prompt(void);
 int get_argc(char* command);
 struct envar* find(char* key);
 struct envar* delete(char* key);
-void add_shell_envar(void);
+void add_shell_envar(char* gbsh_path);
+
+// Environment variables (declared in unistd.h)
+extern char **environ;
 
 // Main function
 int main(int argc, char *argv[]) {
@@ -40,8 +44,13 @@ int main(int argc, char *argv[]) {
 	char command[1024];
 	command[1023] = '\0';
 
+	// Get path of gbsh
+	char gbsh_path[ENVAR_VAR_LENGTH];
+	getcwd(gbsh_path, ENVAR_VAR_LENGTH);
+	strncat(gbsh_path, "/gbsh", 5);
+
 	// Add initial 'shell' envar
-	add_shell_envar();
+	add_shell_envar(gbsh_path);
 
 	// Main loop
 	while (1)
@@ -241,6 +250,113 @@ int main(int argc, char *argv[]) {
 		}
 		else
 		{
+			int argc = get_argc(command);
+
+			if (argc < 0)
+			{
+				// No command provided. Do nothing.
+			}
+			else
+			{
+				// Assume program invocation
+				pid_t pid = fork();
+
+				if (pid == -1)
+				{
+					// Error, failed to fork()
+					perror("Could not fork. Error:");
+				} 
+				else if (pid > 0)
+				{
+					// Parent process waits for the child
+					int status;
+					waitpid(pid, &status, 0);
+					printf("Child return status: %d\n", status);
+				}
+				else 
+				{
+					// Child process executes the program
+					char* pch;		// Dummy char pointer for strtok
+					char* path;		// Path pointer
+					char** argv;	// Argument array pointer
+					// char* envp[2];	// Environment variable array pointer
+
+					// Get first token (in this case command path)
+					pch = strtok(command, " \n\t");
+					// Save executable path
+					path = malloc(strlen(pch)+1);
+					strncpy(path, pch, strlen(pch));
+
+					// Allocate memory for argument string pointers
+					argv = malloc((argc + 2) * sizeof(char*)); // arguments + 2 for the executable itself and NULL pointer
+
+					// NULL pointer check
+					if (argv != NULL)
+					{
+						// First element of argument array is the path itself
+						argv[0] = malloc(strlen(pch)+1);
+						strcpy(argv[0], pch);
+						int i=1;
+						// Allocate memory for strings
+						for (i=1; i < argc+1; i++)
+						{
+							// Get next token (in this case argument)
+							pch = strtok(NULL, " \n\t");
+							// Allocate memory
+							argv[i] = malloc(strlen(pch)+1);
+							// Copy string
+							strcpy(argv[i], pch);
+
+							printf("Argument %d: %s\n", i, argv[i]);
+						}
+						// NULL terminate the array
+						argv[++i] = NULL;
+					}
+					else
+					{
+						printf("Could not malloc.\n");
+					}
+    				
+    				/* TODO: How to pass environment variables?
+    					execve does not execute without specifying the path.
+    					Also passing PATH as an envar does not help.
+    					A possibility: adding the required 'parent' envar to char** environ.
+    					*/
+    				/*
+					extern char** environ;
+					envp = malloc(sizeof(environ) + 1*sizeof(char*));
+					memcpy(&envp, &environ, sizeof(environ));*/
+
+					/*
+					// Add environment variable 'parent'
+					envp[0] = malloc(strlen(gbsh_path) + 10); // 10 for additional envar name
+					strcpy(envp[0], "parent=");
+					strcat(envp[0], gbsh_path);
+					envp[1] = malloc(50);
+					strcpy(envp[1], "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin");
+
+					char *env_args[] = { "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin", "USER=boraozgen", NULL };
+
+					printf("environment variable: %s\n", envp[0]);
+					// execve(path, argv, env_args);
+					*/
+					
+					// Problems when executing with argv[0] instead of path. Why?
+					execvp(path, argv);
+					perror("exec error");
+					_exit(EXIT_FAILURE);   // exec never returns
+
+					// Free allocated memory
+					free(path);
+					for(int i=0; i < argc+2; i++)
+					{
+						free(argv[i]);
+					}
+					free(argv);
+				}
+			}
+			
+			/* Assigment 1 - deprecated
 			// Split input into tokens
 			char * pch;
 			pch = strtok(command, " \n\t");
@@ -249,6 +365,7 @@ int main(int argc, char *argv[]) {
 				printf("%s\n", pch);
 				pch = strtok(NULL, " \n\t");
 			}
+			*/
 		}
 	}
 
@@ -342,48 +459,47 @@ struct envar* find(char* key)
 // Delete an environment variable with given string key
 struct envar* delete(char* key) {
 
-   //start from the first link
-   struct envar* current = head;
-   struct envar* previous = NULL;
-	
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
+	//start from the first link
+	struct envar* current = head;
+	struct envar* previous = NULL;
 
-   //navigate through list
-   while(strncmp(current->variable, key, ENVAR_VAR_LENGTH)) {
+	//if list is empty
+	if(head == NULL) {
+	  return NULL;
+	}
 
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //store reference to current link
-         previous = current;
-         //move to next link
-         current = current->next;
-      }
-   }
+	//navigate through list
+	while(strncmp(current->variable, key, ENVAR_VAR_LENGTH)) {
 
-   //found a match, update the link
-   if(current == head) {
-      //change first to point to next link
-      head = head->next;
-   } else {
-      //bypass the current link
-      previous->next = current->next;
-   }    
-	
-   return current;
+	  //if it is last node
+	  if(current->next == NULL) {
+	     return NULL;
+	  } else {
+	     //store reference to current link
+	     previous = current;
+	     //move to next link
+	     current = current->next;
+	  }
+	}
+
+	//found a match, update the link
+	if(current == head) {
+	  //change first to point to next link
+	  head = head->next;
+	} else {
+	  //bypass the current link
+	  previous->next = current->next;
+	}
+
+	// Free allocated memory
+	free(current);
+
+	return current;
 }
 
 // Add initial shell envar
-void add_shell_envar(void)
+void add_shell_envar(char* gbsh_path)
 {
-	char executable_path[ENVAR_VAR_LENGTH];
-	getcwd(executable_path, ENVAR_VAR_LENGTH);
-	strncat(executable_path, "/gbsh", 5);
-
 	// Create new environment variable node
 	struct envar *link = (struct envar*) malloc(sizeof(struct envar));
 	// Place null terminators for security
@@ -391,7 +507,7 @@ void add_shell_envar(void)
 	link->value[ENVAR_VALUE_LENGTH] = '\0';
 	// Copy strings to envar
 	strncpy(link->variable, "shell", ENVAR_VAR_LENGTH);
-	strncpy(link->value, executable_path, ENVAR_VALUE_LENGTH);
+	strncpy(link->value, gbsh_path, ENVAR_VALUE_LENGTH);
 	
 	// Point it to old first node
 	link->next = head;
